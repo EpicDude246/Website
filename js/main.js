b39d2a27ae51d61942fac4f5c70682d83d3d8014
awsomehacker25 <86263928+awsomehacker25@users.noreply.github.com>
2021-06-28 15:40:20 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,14 +46,3 @@ $(document).ready(function () {
     AOS.init();
 
 });
-
-function copy() {
-    var copyText = document.getElementById("myInput");
-  
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-  
-    document.execCommand("copy");
-  
-    alert("Copied the text: " + copyText.value);
-  }
